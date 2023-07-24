2207b826893ad986cda3c3769d47197b12ca52d5
raghu <raghuvlog295@gmail.com>
2023-07-24 06:51:05 +0545

    Profile Page Completed

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('create-blog', create_blog, name="add_your_blog"),
     path('blog-detail/<int:blog_id>', blog_detail, name="blog_detail_page"),
     path('logout', log_out, name="user_logout"),
+    path('blog-remove/<int:blog_id>', blog_delete, name="blog_delete_page"),
+    path('blog-update/<int:blog_id>', blog_update, name="blog_update_page"),
+    path('profile', profile_page, name="profile_page"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,16 @@ from blog.helper import save_file
 from blog.models import Profile, Blog, User
 from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.contrib.admin.views.decorators import user_passes_test
 
 # Create your views here.
 
 def home(request):
     message = "Hello Everyone Welcome to my Blogging Page"
     context = {"message": message}
+    # set value on session
+    request.session["blog"] = "Welcome to my Blogging Page"
     return render(request, "home.html", context)
 
 
@@ -32,7 +36,11 @@ def log_in(request):
         user = authenticate(request, username =username, password= password)
         if user is not None:
             login(request, user)
-            return redirect("home")
+            profile_pic = None
+            if Profile.objects.filter(user__email=request.user.email).exists():
+                profile_pic = Profile.objects.get(user__email=request.user.email).profile_image
+            request.session["profile_pic"] = profile_pic
+            return redirect("profile_page")
 
         else:
             messages.error(request, message="Email or Password is incorrect")
@@ -71,15 +79,28 @@ def about(request):
     return render(request, "about.html")
 
 def contact(request):
+    # get vlaue on session
+    print("request.session:", request.session.get("blog"))
     return render(request, "contact.html")
 
 
 def blog(request):
-    blog = Blog.objects.all().order_by("-created_at")
-    context = {"blog": blog}
+    search_string = request.GET.get("q")
+    print("search_string:", search_string)
+    if search_string:
+        blog = Blog.objects.filter(title__icontains=search_string)
+    else:
+        blog = Blog.objects.all().order_by("-created_at")
+    has_data = len(blog) > 0
+    page_number = request.GET.get("page", 1)
+    per_page = request.GET.get("per_page", 2)
+    paginator = Paginator(blog, per_page)
+    blog_with_pagination = paginator.get_page(page_number)
+    context = {"blog_with_pagination": blog_with_pagination, "per_page": per_page, "has_data": has_data}
     return render(request, "blog.html", context)
 
 @login_required
+@user_passes_test(lambda user: user.is_superuser)
 def create_blog(request):
     if request.method == "POST":
         title = request.POST.get("title")
@@ -102,3 +123,60 @@ def blog_detail(request, blog_id):
 def log_out(request):
     logout(request)
     return HttpResponseRedirect("/login")
+
+@login_required
+@user_passes_test(lambda user: user.is_superuser)
+def blog_delete(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    blog.delete()
+    messages.info(request, message="Blog deleted Successfully")
+    return redirect("blog")
+
+
+
+@login_required
+@user_passes_test(lambda user: user.is_superuser)
+def blog_update(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("content")
+        blog_image = request.FILES.get("blog_image")
+        image_url = save_file(request, blog_image)
+
+
+        if title:
+            blog.title = title
+        if description:
+            blog.description = description
+        if image_url:
+            blog.image = image_url
+        blog.save()
+        messages.info(request, message="Blog updated Successfully")
+        return redirect("blog")
+    return render(request, "edit-blog.html", {"blog": blog})
+
+
+@login_required
+def profile_page(request):
+    # get value on session
+    print("request.session:", request.session.get("blog"))
+    profile = {}
+    if Profile.objects.filter(user__email=request.user.email).exists():
+        profile = Profile.objects.get(user__email=request.user.email)
+        if request.method == "POST":
+            contact = request.POST.get("contact")
+            address = request.POST.get("address")
+            profile_image = request.FILES.get("profile_image")
+            if profile_image:
+                profile_image_url = save_file(request, profile_image)
+                profile.profile_image = profile_image_url
+            if contact:
+                profile.contact = contact
+            if address:
+                profile.address = address
+            profile.save()
+            messages.info(request, message="Profile updated Successfully")
+            return redirect("profile_page")
+    context = {"profile": profile}
+    return render(request, "profile.html", context)
